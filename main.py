71e8c064ab7470c5ef215ed88be84c2e20504662
fireninja384 <99shivamsinghal@gmail.com>
2025-10-28 10:35:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques/data
-     
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt  
 
 df = pd.read_csv("train.csv");
 
